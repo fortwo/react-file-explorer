ce3c9e385cf4142a5343039aeb23ddc2e4263a5d
Francesco Fortunati <fortunati.francesco89@gmail.com>
2018-02-16 18:33:03 +0100

    feature(Menu): basic nested menu

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -12,22 +12,50 @@ const Wrapper = styled.div`
   background: white;
   border: 1px solid red;
   padding: 8px;
-  display: flex;
   top: ${props => props.top};
   left: ${props => props.left};
 `;
 
-const FirstLevel = styled.ul`
-  width: 100px;
+const FirstLevelWrapper = styled.ul`
   margin: 0;
   padding: 0;
   list-style: none;
+  z-index: 1;
 
   > li {
     cursor: default;
 
+    &:hover {
+      background: orange;
+    }
+
     &.selected {
       background: red;
+    }
+  }
+
+  > .first-level {
+    position: relative;
+  }
+
+  > .first-level > .nested-level {
+    display: none;
+    position: absolute;
+    top: 0;
+    left: 0;
+    background: white;
+    border: 1px solid red;
+    padding: 8px;
+  }
+
+  > .first-level:hover > .nested-level {
+    display: block;
+    list-style: none;
+    left: 40px;
+    z-index: 2;
+
+    > li:hover {
+      background: orange;
     }
   }
 `;
@@ -42,20 +70,35 @@ class Menu extends Component {
         top={position.y}
         left={position.x}>
 
-        <FirstLevel>
-          <li
-            onClick={() => this.props.toggleViewMode(ViewModes.LIST)}
-            className={viewMode === ViewModes.LIST && 'selected'}>List</li>
-          <li
-            onClick={() => this.props.toggleViewMode(ViewModes.SMALL_ICONS)}
-            className={viewMode === ViewModes.SMALL_ICONS && 'selected'}>Small Icons</li>
-          <li
-            onClick={() => this.props.toggleViewMode(ViewModes.MEDIUM_ICONS)}
-            className={viewMode === ViewModes.MEDIUM_ICONS && 'selected'}>Medium Icons</li>
-          <li
-            onClick={() => this.props.toggleViewMode(ViewModes.LARGE_ICONS)}
-            className={viewMode === ViewModes.LARGE_ICONS && 'selected'}>Large Icons</li>
-        </FirstLevel>
+        <FirstLevelWrapper>
+          <li className='first-level'>
+            <div>View</div>
+            <ul className='nested-level'>
+              <li
+                onClick={() => this.props.toggleViewMode(ViewModes.LIST)}
+                className={viewMode === ViewModes.LIST && 'selected'}>List</li>
+              <li
+                onClick={() => this.props.toggleViewMode(ViewModes.SMALL_ICONS)}
+                className={viewMode === ViewModes.SMALL_ICONS && 'selected'}>Small Icons</li>
+              <li
+                onClick={() => this.props.toggleViewMode(ViewModes.MEDIUM_ICONS)}
+                className={viewMode === ViewModes.MEDIUM_ICONS && 'selected'}>Medium Icons</li>
+              <li
+                onClick={() => this.props.toggleViewMode(ViewModes.LARGE_ICONS)}
+                className={viewMode === ViewModes.LARGE_ICONS && 'selected'}>Large Icons</li>
+            </ul>
+          </li>
+
+          <li className='first-level'>
+            <div>Sort by</div>
+            <ul className='nested-level'>
+              <li>Name</li>
+              <li>Size</li>
+              <li>Type</li>
+              <li>Last edit</li>
+            </ul>
+          </li>
+        </FirstLevelWrapper>
 
       </Wrapper>
     );
